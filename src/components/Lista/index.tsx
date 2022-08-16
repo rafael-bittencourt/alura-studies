6e180fc6668b91599847e3e9305aa7676c02e6f2
Rafael <rb2335@gmail.com>
2022-08-16 18:24:23 -0300

    Versão Final projeto

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,30 +1,29 @@
-import Item from "./Item";
+import React from 'react';
+import { ITarefa } from '../../types/tarefa';
+import Item from './Item';
 import style from './Lista.module.scss';
 
-function Lista () {
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00' 
-    }, {
-        tarefa: 'Typescript',
-        tempo: '03:00:00' 
-    }];
-    return (
-        <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
-            <ul>
-                {tarefas.map((item, index) => (
-                    <Item
-                        key={index}
-                        {...item}
-                    />
-                ))}
-            </ul>
-        </aside>
-    );
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+}
+
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map(item => (
+          <Item
+            selecionaTarefa={selecionaTarefa}
+            key={item.id}
+            {...item}
+          />
+        ))}
+      </ul>
+    </aside>
+  )
 }
 
 export default Lista;
